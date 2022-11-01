155fb19740397dc21fdb438ab4a4a2cf478bd817
Shibendra Bhattacharjee <storebshiv@gmail.com>
2022-11-01 18:47:54 +0530

    data ingestion config and data schema added

---
+++ activity/constant/training_pipeline/__init__.py
@@ -0,0 +1,67 @@
+import os
+
+from activity.constant.s3_bucket import TRAINING_BUCKET_NAME
+
+TARGET_COLUMN = "Activity"
+PIPELINE_NAME: str = "activity"
+ARTIFACT_DIR: str = "artifact"
+
+'''
+Defining basic and common file names
+'''
+
+FILE_NAME: str = "activity.csv"
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+MODEL_FILE_NAME = "model.pkl"
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
+SCHEMA_DROP_COLS = "drop_columns"
+
+'''
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+'''
+DATA_INGESTION_COLLECTION_NAME: str = "car"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
