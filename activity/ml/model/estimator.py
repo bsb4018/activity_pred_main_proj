a14032caf817513cb10e97d82d12712b20b3e55f
Shibendra Bhattacharjee <storebshiv@gmail.com>
2022-11-07 13:52:20 +0530

    Model Evaluation Stage Completed

--- activity/ml/model/estimator.py
+++ activity/ml/model/estimator.py
@@ -1,25 +1,35 @@
-import sys
+import os,sys
 from pandas import DataFrame
 from activity.exception import ActivityException
 from activity.logger import logging
 from sklearn.pipeline import Pipeline
+from activity.constant.training_pipeline import SAVED_MODEL_DIR, MODEL_FILE_NAME
 
 class TargetValueMapping:
     def __init__(self):
-        self.LAYING: int = 0
-        self.SITTING: int = 1
-        self.STANDING: int = 2
-        self.WALKING: int = 3
-        self.WALKING_DOWNSTAIRS: int = 4
-        self.WALKING_UPSTAIRS: int = 5
+        try:
+            self.LAYING: int = 0
+            self.SITTING: int = 1
+            self.STANDING: int = 2
+            self.WALKING: int = 3
+            self.WALKING_DOWNSTAIRS: int = 4
+            self.WALKING_UPSTAIRS: int = 5
+
+        except Exception as e:
+            raise e
 
     def to_dict(self):
-        return self.__dict__
+        try:
+            return self.__dict__
+        except Exception as e:
+            raise e    
 
     def reverse_mapping(self):
-        mapping_response = self.to_dict()
-        return dict(zip(mapping_response.values(), mapping_response.keys()))
-
+        try:
+            mapping_response = self.to_dict()
+            return dict(zip(mapping_response.values(), mapping_response.keys()))
+        except Exception as e:
+            raise e
 
 class ActivityModel:
     def __init__(self,preprocessor,model):
@@ -34,5 +44,42 @@ class ActivityModel:
             x_transform = self.preprocessor.transform(x)
             y_hat = self.model.predict(x_transform)
             return y_hat
+        except Exception as e:
+            raise e
+
+
+class ModelResolver:
+    def __init__(self, model_dir=SAVED_MODEL_DIR):
+        try:
+            self.model_dir = model_dir
+
+        except Exception as e:
+            raise e
+
+    def get_best_model_path(self,) -> str:
+        try:
+            timestamps = list(map(int, os.listdir(self.model_dir)))
+            latest_timestamp = max(timestamps)
+            latest_model_path = os.path.join(self.model_dir, f"{latest_timestamp}", MODEL_FILE_NAME)
+            return latest_model_path       
+        
+        except Exception as e:
+            raise e
+
+    def is_model_exists(self) -> bool:
+        try:
+            if not os.path.exists(self.model_dir):
+                return False
+
+            timestamps = os.listdir(self.model_dir)
+            if len(timestamps) == 0:
+                return False
+
+            latest_model_path = self.get_best_model_path()
+            if not os.path.exists(latest_model_path):
+                return False
+
+            return True
+
         except Exception as e:
             raise e
--- activity/pipeline/train_pipeline.py
+++ activity/pipeline/train_pipeline.py
@@ -3,9 +3,10 @@ import sys
 from activity.components.data_ingestion import DataIngestion
 from activity.components.data_validation import DataValidation
 from activity.components.data_transformation import DataTransformation
+from activity.components.model_evaluation import ModelEvaluation
 from activity.components.model_trainer import ModelTrainer
-from activity.entity.config_entity import DataIngestionConfig, DataTransformationConfig, DataValidationConfig, ModelTrainerConfig, TrainingPipelineConfig
-from activity.entity.artifact_entity import DataIngestionArtifact, DataTransformationArtifact, DataValidationArtifact
+from activity.entity.config_entity import DataIngestionConfig, DataTransformationConfig, DataValidationConfig, ModelEvaluationConfig, ModelTrainerConfig, TrainingPipelineConfig
+from activity.entity.artifact_entity import DataIngestionArtifact, DataTransformationArtifact, DataValidationArtifact, ModelTrainerArtifact
 from activity.exception import ActivityException
 from activity.logger import logging
 
@@ -84,9 +85,19 @@ class TrainPipeline:
             model_trainer_artifact = model_trainer.initiate_model_trainer()
             return model_trainer_artifact
         except  Exception as e:
-            raise  ActivityException(e,sys)
+            raise  ActivityException(e,sys) from e
 
-   
+    def start_model_evaluation(self,data_validation_artifact:DataValidationArtifact,
+                                 model_trainer_artifact:ModelTrainerArtifact,
+                                ):
+        try:
+            model_eval_config = ModelEvaluationConfig(self.training_pipeline_config)
+            model_eval = ModelEvaluation(model_eval_config, data_validation_artifact, model_trainer_artifact)
+            model_eval_artifact = model_eval.initiate_model_evaluation()
+            return model_eval_artifact
+        except  Exception as e:
+            raise  ActivityException(e,sys) from e
+
     def run_pipeline(self,) -> None:
         try:
             TrainPipeline.is_pipeline_running=True
@@ -94,6 +105,9 @@ class TrainPipeline:
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
             model_trainer_artifact = self.start_model_trainer(data_transformation_artifact)
-
+            model_eval_artifact = self.start_model_evaluation(data_validation_artifact, model_trainer_artifact)
+            if not model_eval_artifact.is_model_accepted:
+                raise Exception("Trained model is not better than the best model")
+        
         except Exception as e:
             raise ActivityException(e, sys) from e
