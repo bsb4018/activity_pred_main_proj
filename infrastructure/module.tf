8a598af54001656bea9f09c5c969f847c518e244
Shibendra Bhattacharjee <storebshiv@gmail.com>
2022-12-09 03:43:36 +0530

    Added IaaS:Terraform service

---
+++ infrastructure/module.tf
@@ -0,0 +1,33 @@
+terraform {
+   backend "s3" {
+    bucket = "bsb4018-s3-activitypred"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+  }
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.4.3"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.45.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "sensor_model" {
+  source = "./sensor_model_bucket"
+}
+
+module "sensor_ecr" {
+  source = "./sensor_ecr"
+}
+
+#module "sensor_ec2" {
+#  source = "./sensor_ec2"
+#}
